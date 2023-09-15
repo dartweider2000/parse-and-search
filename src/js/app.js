ec290eaa23e54c2eace4c78b4bced82bc96c1c6c
dartweider2000 <nikita.ivanov574477333@gmail.com>
2023-09-15 08:23:37 +0300

    it is alive

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 //console.log('!!!');
 document.addEventListener('DOMContentLoaded', async (e) => {
-   const waitForGoogle = async (ms) => {
+   const waitFor = async (ms = 0, target = null , type = 1) => {
       return new Promise((resolve, reject) => {
          let count = 0;
    
          const interval = setInterval(() => {
-            if(google){
+            if(type == 1 && google || type == 2 && document.querySelector(target)){
                clearInterval(interval);
                resolve();
             }
@@ -19,47 +19,64 @@ document.addEventListener('DOMContentLoaded', async (e) => {
       });
    }
 
-   await waitForGoogle(10);
+   const wait = async ms => new Promise(resolve => setTimeout(resolve, ms));
+
+   const isSearch = location.pathname.includes('search');
+   await Promise.all([waitFor(), waitFor(0, '.gsc-cursor-box', 2)]);
+
+   if(!isSearch){
+      document.querySelectorAll('.gsc-tabsArea>div')[1].dispatchEvent(new Event('click'));
+
+      await waitFor(0, '.gsc-resultsbox-visible>.gsc-resultsRoot:last-child .gsc-cursor-box', 2);
+
+
+      // document.querySelector()
+      // console.log(document.querySelector('.gsc-resultsbox-visible>.gsc-resultsRoot:last-child .gsc-cursor-box'));
+   }
+
+   const resultContainer = document.querySelectorAll('.gsc-resultsRoot')[isSearch ? 0 : 1];
+
+   const overlay = document.querySelector('.gsc-results-wrapper-overlay');
+   overlay.classList.remove('gsc-results-wrapper-overlay');
+   overlay.style.position = 'static !important';
+
+   const contentBlock = resultContainer.querySelector('.gsc-expansionArea');
+   const contentPagination = resultContainer.querySelector('.gsc-cursor-box');
+
+   if(contentBlock.querySelector('.gsc-imageResult')){
+      contentBlock.classList.add('image-content');
+   }
+
+   document.querySelector('#___gcse_0').remove();
+
+   const wrapper = document.createElement('div');
+   wrapper.append(contentBlock, contentPagination);
+   wrapper.classList.add('.my-wrapper');
 
    const header = document.querySelector('header');
-   const googleContainer = header.parentElement.nextElementSibling;
-   googleContainer.style.background = 'inherit';
-   //googleContainer.style.overflow = 'auto';
-   // gsc-wrapper
-   //document.querySelector('.gsc-wrapper').style.overflow = 'auto';
-   //gsc-resultsbox-visible
-   //document.querySelector('.gsc-control-wrapper-cse').style.overflow = 'auto';
-   //document.querySelector('.gsc-results-wrapper-visible').style.overflow = 'auto';
-   console.log(googleContainer);
+   wrapper.style.marginTop = `${header.offsetHeight}px`;
 
-   //document.querySelector('.gsc-control-cse').style.overflow = 'auto';
-   //gsc-expansionArea
-   //document.querySelector('.gsc-expansionArea').style.overflow = 'auto';
-   googleContainer.style.marginTop = `${header.offsetHeight}px`;
 
-   const content = document.querySelector('.gsc-results-wrapper-overlay');
-   content.classList.remove('gsc-results-wrapper-overlay');
-   content.style.position = 'static !important';
-   //content.style.overflow = 'auto';
+   contentPagination.addEventListener('click', e => {
+      const el = e.target;
+      const activeClass = 'gsc-cursor-current-page';
 
-   // if(google){
-   //    console.log(google);
-   // }
-   //const google = document.querySelector('#___gcse_0');
-   
-   //google.style.marginTop = `${header.offsetHeight}px`;
-   
-   //console.log(google.search.element.getElement());
+      if(el.closest('.gsc-cursor-page') && !el.closest(`.gsc-cursor-page.${activeClass}`)){
+         const link = el.closest('.gsc-cursor-page');
 
-   // console.log('!!!');
-   // console.log(document.body.children[1]);
-   // console.log(document.body.children['___gcse_0']);
-   // console.log(document.body.querySelector('#___gcse_0'));
+         document.querySelector(`.${activeClass}`).classList.remove(activeClass);
+         link.classList.add(activeClass);
 
-//gsc-results-wrapper-overlay gsc-results-wrapper-visible
+         window.scrollTo({
+            'left': 0,
+            'top': 0,
+            'behavior': 'smooth'
+         });
+      }
+   })
+
+
+   header.parentElement.after(wrapper);
 });
 
-//console.log(document.body.querySelector('.gsc-results-wrapper-overlay'));
-// console.log(document.body.children['___gcse_0']);
-// console.log(document.body.querySelector('#___gcse_0'));
 
